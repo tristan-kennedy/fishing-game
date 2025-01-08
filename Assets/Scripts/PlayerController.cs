423d84f68eaa7e9f20b9d7d8c50758929a2d736c
Tristan Kennedy <tdouglaskennedy@gmail.com>
2025-01-07 23:44:53 -0600

    Camera follows and player moves

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    private Vector2 move;
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        move = context.ReadValue<Vector2>();
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movePlayer();
+    }
+
+    public void movePlayer() 
+    {
+        var matrix = Matrix4x4.Rotate(Quaternion.Euler(0, 45, 0));
+        var movement = matrix.MultiplyPoint3x4(new Vector3(move.x, 0f, move.y));
+
+        if (movement != Vector3.zero)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15f);
+        }
+
+        transform.Translate(movement * speed * Time.deltaTime, Space.World);
+    }
+}
